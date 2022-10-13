6880a67379c82fd891ceaaf364fafa202cc068d0
Tomas C Imach <tomas90@gmail.com>
2022-10-13 11:26:55 -0300

    site commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  publicRuntimeConfig: {
+    url: 'http://127.0.0.1:8055/',
+  }
+}
+
+module.exports = nextConfig
