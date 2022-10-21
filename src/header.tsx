7d0db6a1b1c4be4ef3fb7796c00f65e3ec220565
nickandless0 <nickandless0@gmail.com>
2022-10-21 10:16:54 -0300

    create login page

--- src/header.tsx
+++ src/header.tsx
@@ -1,50 +1,53 @@
 
 export default function Header() {
   return (
-        <header className="bg-white header header-md navbar navbar-fixed-top box-shadow min_width center">
-          <div className="navbar-header aside-md dk">
-            <a href="/" className="navbar-brand">
-              {/*<img src="images/logo.png" className="m-r-sm" alt="scale" />*/}
-              <span className="hidden-nav-xs">LOOP</span>
-            </a>
-            <a
-              className="btn btn-link visible-xs"
-              data-toggle="dropdown"
-              data-target=".user"
-            >
-              <span className="thumb-sm avatar pull-left">
-                <img src="https://via.placeholder.com/50" alt="..." />
-              </span>
-              <b className="caret"></b>
-            </a>
-          </div>
-          <ul className="nav navbar-nav navbar-right m-n hidden-xs nav-user user">
-            <li className="dropdown">
-              <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                <span className="thumb-sm avatar pull-left">
-                  <img src="https://via.placeholder.com/50" alt="..." />
-                </span>
-                <b className="caret"></b>
+    <header className="bg-white header header-md navbar navbar-fixed-top box-shadow min_width center">
+      <div className="navbar-header aside-md dk">
+        <a href="/" className="navbar-brand">
+          {/*<img src="images/logo.png" className="m-r-sm" alt="scale" />*/}
+          <span className="hidden-nav-xs">LOOP</span>
+        </a>
+        <a
+          className="btn btn-link visible-xs"
+          data-toggle="dropdown"
+          data-target=".user"
+        >
+          <span className="thumb-sm avatar pull-left">
+            <img src="https://via.placeholder.com/50" alt="..." />
+          </span>
+          <b className="caret"></b>
+        </a>
+      </div>
+      <ul className="nav navbar-nav navbar-right m-n hidden-xs nav-user user">
+        <li className="dropdown">
+          <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+            <span className="thumb-sm avatar pull-left">
+              <img src="https://via.placeholder.com/50" alt="..." />
+            </span>
+            <b className="caret"></b>
+          </a>
+          <ul className="dropdown-menu animated fadeInRight">
+            <li>
+              <a href="/login">Register</a>
+            </li>
+            <li>
+              <a href="/profile">Profile</a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="badge bg-danger pull-right">3</span>
+                Notifications
               </a>
-              <ul className="dropdown-menu animated fadeInRight">
-                <li>
-                  <a href="/profile">Profile</a>
-                </li>
-                <li>
-                  <a href="#">
-                    <span className="badge bg-danger pull-right">3</span>
-                    Notifications
-                  </a>
-                </li>
-                <li className="divider"></li>
-                <li>
-                  <a href="modal.lockme.html" data-toggle="ajaxModal">
-                    Logout
-                  </a>
-                </li>
-              </ul>
+            </li>
+            <li className="divider"></li>
+            <li>
+              <a href="modal.lockme.html" data-toggle="ajaxModal">
+                Logout
+              </a>
             </li>
           </ul>
-        </header>
+        </li>
+      </ul>
+    </header>
   );
 }
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+export default function Login() {
+    return (
+        <section id="content">
+            <div className="panel-body">
+                <div className="clearfix m-t">
+                    <div className="inline">
+                        <div className="h4 m-t m-b-xs">Log In</div>
+                        <small className="text-muted m-b">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget nibh rutrum, porta sapien a, maximus arcu.</small>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
